e15a8e94d5a6a31b732210c9e331cb43c69747c7
Artur <artur.halat97@gmail.com>
2019-11-29 11:38:59 +0100

    New mediaQ

--- firstphp.php
+++ firstphp.php
@@ -2,9 +2,16 @@
 <html>
 <body>
     <?php
-       ECHO "Tekst HTML<br>";
-       echo "Tekst HTML<br>";
-       EcHo "Tekst HTML<br>";
+      $s = "To JEST \"przykładowy\" <b>ciąg 'tekstowy'</b>";
+      echo "\n 1 " . $s ;
+      echo "\n 2 " . addslashes($s);
+      echo "\n 3 " . crc32($s) ;
+      echo "\n 4 " . explode(" ", $s) ;
+      echo "\n 5 " . htmlspecialchars($s) ;
+      echo "\n 6 " . htmlspecialchars_decode (htmlspecialchars($s)) ;
+      echo "\n 7 " . implode (" ", explode(" ", $s)) ;
+      echo "\n 8 " . md5($s) ;
+      echo "\n 9 " . sha1($s) ;
        
     ?>
 </body>
